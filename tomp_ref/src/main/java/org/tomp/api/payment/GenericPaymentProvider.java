d72eccec91f6dcd5853719a4fdbbb72adeba9b6b
Kevin Kuhfeldt <kevin.kuhfeldt@gmx.de>
2021-09-17 09:17:01 +0200

    update journal entries

--- tomp_ref/src/main/java/org/tomp/api/payment/GenericPaymentProvider.java
+++ tomp_ref/src/main/java/org/tomp/api/payment/GenericPaymentProvider.java
@@ -1,14 +1,12 @@
 package org.tomp.api.payment;
 
 import java.math.BigDecimal;
-import java.util.Currency;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.model.AssetType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
@@ -49,6 +47,8 @@ public class GenericPaymentProvider implements PaymentProvider {
 			@NotNull @Valid OffsetDateTime from, @NotNull @Valid OffsetDateTime to, JournalState state, String category,
 			String id, String maasId) {
 
+		List<JournalEntry> journalEntries = new ArrayList<>();
+
 		JournalEntry invoice = new JournalEntry();
 		invoice.setAmount(new BigDecimal("4.76"));
 		invoice.setAmountExVat(new BigDecimal("4"));
@@ -56,13 +56,16 @@ public class GenericPaymentProvider implements PaymentProvider {
 		invoice.setCurrencyCode("EUR");
 		invoice.setDetails("Invoice for your tomp trip");
 		//invoice.setExpirationDate();
-		invoice.setJournalId(UUID.randomUUID().toString());
+		invoice.setJournalId(id);
 		invoice.setState(JournalState.TO_INVOICE);
 		invoice.setVatCountryCode("DE");
 		invoice.setVatRate(new BigDecimal("19"));
 		invoice.setUsedTime(new BigDecimal("15"));
+		invoice.setDistance(new BigDecimal("200"));
+		invoice.setDistanceType(JournalEntry.DistanceTypeEnum.KM);
 
-		return List.of(invoice);
+		journalEntries.add(invoice);
+		return journalEntries;
 	}
 
 }
